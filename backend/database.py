29686f1239e211dc7dd582c4f49353f8d5a290ab
rajbhandarisameep <rajbhandarisameep@gmail.com>
2026-02-24 10:48:07 +0545

    Updating backend for cloud databse

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,18 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./salveeya.db"
-# If using PostgreSQL in the future, change URL here
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+load_dotenv()
+
+# Use a default SQLite URL if DATABASE_URL is not set in the environment
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./salveeya.db")
+
+# Only use the SQLite specifically `check_same_thread` argument if we are actually using SQLite
+connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} # needed for SQLite
+    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
